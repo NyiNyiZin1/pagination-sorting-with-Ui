0364b26e32f3d8995121b5b8719d73e09600ee11
NyiNyiZin1 <54429851+NyiNyiZin1@users.noreply.github.com>
2020-04-23 16:08:22 +0630

    Update EmployeeEntity.java

--- src/main/java/com/howtodoinjava/demo/model/EmployeeEntity.java
+++ src/main/java/com/howtodoinjava/demo/model/EmployeeEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class EmployeeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     
     @Column(name="first_name")
